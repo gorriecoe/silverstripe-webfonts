baad9fa513313cc4c18daabf2f124094cb67c8ae
Gorrie Coe <gorriecoe@gmail.com>
2018-02-13 21:29:53 +1300

    Removed used classes

--- src/extensions/LeftAndMainTinymcePlugin.php
+++ src/extensions/LeftAndMainTinymcePlugin.php
@@ -3,10 +3,8 @@
 namespace gorriecoe\Webfonts\Extensions;
 
 use gorriecoe\Webfonts\View\Webfonts;
-use SilverStripe\Core\Config\Config;
 use SilverStripe\View\Requirements;
 use SilverStripe\Admin\LeftAndMainExtension;
-use SilverStripe\Core\Manifest\ModuleResourceLoader;
 
 /**
  * Include the TinyMCE TypeKit plugin with the CMS configured TypeKit ID.
