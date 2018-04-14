cb5dff0b0ba3c9ee9acf1d8e754ebcb9ef050f5c
Gorrie Coe <gorriecoe@gmail.com>
2018-04-15 08:57:54 +1200

    Added dummy file to prevent error

--- _config.php
+++ _config.php
@@ -1,5 +1,11 @@
 <?php
 
 use SilverStripe\Forms\HTMLEditor\HTMLEditorConfig;
+use SilverStripe\Core\Manifest\ModuleResourceLoader;
+
 // Enable the Webfont TinyMCE plugin by default.
-HtmlEditorConfig::get('cms')->enablePlugins(['webfonts' => null]);
+HtmlEditorConfig::get('cms')->enablePlugins([
+    'webfonts' => ModuleResourceLoader::singleton()->resolvePath(
+        'gorriecoe/silverstripe-webfonts: assets/js/tinymce.webfontsdummy.js'
+    )
+]);
---
+++ assets/js/tinymce.webfontsdummy.js
@@ -0,0 +1,1 @@
+// Dummy file to prevent error from unfound file.  Yeah this is a bit hacky.
